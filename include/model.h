10342cbf8b84ca0088b6b59e4612837f856cb896
Ramkumar Natarajan <nrkumar93@gmail.com>
2021-11-16 19:03:28 -0500

    added mujoco api stuff

--- include/model.h
+++ include/model.h
@@ -2,17 +2,38 @@
 #define _DYNAMICS_H_
 
 #include "common.h"
+#include "config.h"
+#include "mujoco_interface.h"
 
 class Model {
 public:
   virtual VectorXd dynamics(const VectorXd& x, const VectorXd& u) = 0;
+  virtual VectorXd dynamics(const VectorXd& x, const VectorXd& u, bool step) {};
+  virtual VecOfVecXd findInitU(VectorXd& x0, VectorXd& xF, int N, double dt) {};
   virtual double cost(const VectorXd& x, const VectorXd& u) = 0;
   virtual double final_cost(const VectorXd& x) = 0;
 
+  virtual void setGoal(VectorXd& g) {};
+
   VectorXd integrate_dynamics(const VectorXd& x, const VectorXd& u, double dt) {
-    VectorXd x1 = x + dynamics(x,u)*dt;
+
+    VectorXd x1(x_dims);
+
+    if (use_mujoco)
+    {
+      x1 = dynamics(x, u, true);
+      return x1;
+    }
+
+    x1 = x + dynamics(x,u)*dt;
     return x1;
   }
+
+  MujocoPsopt mj_handle;
+  mjModel* mj_model;
+  mjData* mj_data;
+
+  bool use_mujoco = false;
 
   VectorXd u_min, u_max;
 
